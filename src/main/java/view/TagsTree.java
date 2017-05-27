da052bc1bd9ab437c4f62a578ef1e659bfb258d9
leafunes <funes.s.leandro@gmail.com>
2017-05-27 03:13:11 -0300

    Se ordeno un poco el codigo de TagsTree

--- src/main/java/view/TagsTree.java
+++ src/main/java/view/TagsTree.java
@@ -4,7 +4,6 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import com.vaadin.data.util.HierarchicalContainer;
 import com.vaadin.event.Transferable;
 import com.vaadin.event.dd.DragAndDropEvent;
 import com.vaadin.event.dd.DropHandler;
@@ -27,7 +26,9 @@ public class TagsTree extends Panel{
 	private TagService tagService;
 	private Tag tagSelected;
 	
-	private Map<String, Boolean> isExtended;
+	private TagsEdit editPanel;
+	
+	private Map<String, Boolean> state;
 	
 	private MessageBox messageBox = MessageBox.getMessageBox();
 	
@@ -37,99 +38,15 @@ public class TagsTree extends Panel{
 		
 		tagService = TagService.getService();
 		
-		isExtended = new HashMap<>();
+		this.editPanel = editPanel;
 		
 		tagsTree = new Tree();
 		tagsTree.setSizeFull();
-		
 		tagsTree.setDragMode(TreeDragMode.NODE);
 		
-		tagsTree.setDropHandler(new DropHandler() {
-			
-			@Override
-			public AcceptCriterion getAcceptCriterion() {
-				return AcceptAll.get();
-			}
-			
-			@Override
-			public void drop(DragAndDropEvent event) {
-				 // Wrapper for the object that is dragged
-		        Transferable t = event.getTransferable();
-		        
-
-		        // Make sure the drag source is the same tree
-		        if (t.getSourceComponent() != tagsTree)
-		            return;
-
-		        TreeTargetDetails target = (TreeTargetDetails)
-		            event.getTargetDetails();
-
-		        // Get ids of the dragged item and the target item
-		        Object sourceItemId = t.getData("itemId");
-		        Object targetItemId = target.getItemIdOver();
-		        Object preParent =  tagsTree.getParent(sourceItemId);
-
-		        // On which side of the target the item was dropped
-		        VerticalDropLocation location = target.getDropLocation();
-
-		        HierarchicalContainer container = (HierarchicalContainer)
-		        tagsTree.getContainerDataSource();
-
-	            Object parentId = container.getParent(targetItemId);
-		        
-		        Tag sourceTag = tagService.getTagByName((String)sourceItemId);
-		        Tag targetTag = tagService.getTagByName((String)targetItemId);
-		        Tag parentTag = tagService.getTagByName((String)parentId);
-		        System.out.println(sourceTag.getNombre());
-		        System.out.println(targetTag.getNombre());
-		        System.out.println(parentId);
-		        // Drop right on an item -> make it a child
-		        if (location == VerticalDropLocation.MIDDLE){
-		            tagsTree.setChildrenAllowed(targetItemId, true);
-		            tagsTree.setParent(sourceItemId, targetItemId);
-		            tagService.setPadre(sourceTag, targetTag);
-		        }
-
-		        // Drop at the top of a subtree -> make it previous
-		        else if (location == VerticalDropLocation.TOP) {
-		            container.setParent(sourceItemId, parentId);
-		            tagService.setPadre(sourceTag, parentTag);
-		            if(tagsTree.isRoot(preParent))
-		            	tagsTree.setChildrenAllowed(preParent, false);
-		            container.moveAfterSibling(sourceItemId, targetItemId);
-		            container.moveAfterSibling(targetItemId, sourceItemId);
-		        }
-
-		        // Drop below another item -> make it next
-		        else if (location == VerticalDropLocation.BOTTOM) {
-		            container.setParent(sourceItemId, parentId);
-		            tagService.setPadre(sourceTag, parentTag);
-		            if(tagsTree.isRoot(preParent))
-		            	tagsTree.setChildrenAllowed(preParent, false);
-		            container.moveAfterSibling(sourceItemId, targetItemId);
-		        }
-				
-			}
-		});
+		initDropHandler();
 		
-		tagsTree.addExpandListener(event -> isExtended.put((String)event.getItemId(), true));
-		
-
-		tagsTree.addCollapseListener(event -> isExtended.put((String)event.getItemId(), false));
-		
-		tagsTree.addValueChangeListener(event -> {
-		    if (event.getProperty() != null && event.getProperty().getValue() != null) {
-				String tagSelectedName = (String) event.getProperty().getValue();
-				
-				tagSelected = tagService.getTagByName(tagSelectedName);
-				
-				editPanel.editTag(tagSelected);
-		    }
-		    else{
-		    	tagSelected = null;
-		    }
-		});
-		
+		initListeners();
 		
 		loadTree();
 		
@@ -142,20 +59,15 @@ public class TagsTree extends Panel{
 	
 	private void loadTree(){
 		
+		saveState();
+		
 		tagsTree.removeAllItems();
 
 		List<Tag> tags = tagService.getAllTags();
 		
-		tags.forEach(t -> {
-			tagsTree.addItem(t.getNombre());
-			if(!isExtended.containsKey(t.getNombre()))
-				isExtended.put(t.getNombre(), false);
-		});
+		tags.forEach(t -> tagsTree.addItem(t.getNombre()));
 		
 		for (Tag t: tags) {
-
-			if(isExtended.get(t.getNombre()))
-				tagsTree.expandItem(t.getNombre());
 			
 			tagsTree.setChildrenAllowed(t.getNombre(), false);
 			
@@ -169,26 +81,118 @@ public class TagsTree extends Panel{
 			}
 		}
 		
+		loadState();
+		
 	}
 	
 	public void addTag(String name){
 		
 		tagService.addTag(new Tag(name, tagSelected));
-		
-		isExtended.put(name, false);
 
 		tagsTree.addItem(name);
 		tagsTree.setChildrenAllowed(name, false);
+		tagsTree.select(name);//Para que se edite
 		
 		if(tagSelected != null){
 			tagsTree.setChildrenAllowed(tagSelected.getNombre(), true);
 			tagsTree.setParent(name, tagSelected.getNombre());
 			
 			tagsTree.expandItem(tagSelected.getNombre());
-			isExtended.put(tagSelected.getNombre(), true);
 		}
 		
-		tagsTree.select(name);//Para que se edite
+	}
+	
+	private void initListeners(){
+		
+		tagsTree.addValueChangeListener(event -> {
+
+	    	tagSelected = null;
+			
+		    if (event.getProperty() != null && event.getProperty().getValue() != null) {
+				String tagSelectedName = (String) event.getProperty().getValue();
+				
+				tagSelected = tagService.getTagByName(tagSelectedName);
+				
+				editPanel.editTag(tagSelected);
+		    }
+		});
+		
+	}
+	
+	private void saveState(){
+
+		state = new HashMap<>();
+		
+		tagsTree.getItemIds().forEach( id -> state.put((String)id, tagsTree.isExpanded(id)));
+		
+		state.forEach((id, expanded) -> System.out.println(id + " " + expanded));
+		
+	}
+	
+	private void loadState(){
+		
+		state.forEach((id, expanded) -> { if(expanded) tagsTree.expandItem(id); });
+		
+	}
+	
+	private void initDropHandler(){
+		
+		tagsTree.setDropHandler(new DropHandler() {
+			
+			@Override
+			public AcceptCriterion getAcceptCriterion() {
+				return AcceptAll.get();
+			}
+			
+			@Override
+			public void drop(DragAndDropEvent event) {
+				 // Wrapper for the object that is dragged
+		        Transferable t = event.getTransferable();
+
+		        // Make sure the drag source is the same tree
+		        if (t.getSourceComponent() != tagsTree)
+		            return;
+
+		        TreeTargetDetails target = (TreeTargetDetails)event.getTargetDetails();
+
+		        // Get ids of the dragged item and the target item
+		        Object sourceItemId = t.getData("itemId");
+		        Object targetItemId = target.getItemIdOver();
+		        Object preParentId =  tagsTree.getParent(sourceItemId);
+	            Object parentId = tagsTree.getParent(targetItemId);
+		        
+		        // The item can't move into itself
+		        if(sourceItemId.equals(targetItemId))
+		        	return;
+		        
+		        Tag sourceTag = tagService.getTagByName((String)sourceItemId);
+		        Tag targetTag = tagService.getTagByName((String)targetItemId);
+		        Tag parentTag = tagService.getTagByName((String)parentId);
+		        
+		        if(!tagsTree.hasChildren(preParentId))
+	            	tagsTree.setChildrenAllowed(preParentId, false);
+
+		        // On which side of the target the item was dropped
+		        VerticalDropLocation location = target.getDropLocation();
+
+		        // Drop right on an item -> make it a child
+		        if (location == VerticalDropLocation.MIDDLE){
+		            tagsTree.setChildrenAllowed(targetItemId, true);
+		            tagsTree.setParent(sourceItemId, targetItemId);
+		            tagsTree.expandItem(targetItemId);
+		            
+		            tagService.setPadre(sourceTag, targetTag);
+		        }
+
+		        else{
+		            tagsTree.setParent(sourceItemId, parentId);
+		            tagService.setPadre(sourceTag, parentTag);
+		        }
+		        
+				
+			}
+		});
+		
 	}
 	
 	
